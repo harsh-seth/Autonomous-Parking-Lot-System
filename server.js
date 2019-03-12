ebbd79b9a42035af284b2da6311c529c86828527
Harsh Seth <harshseth2006@gmail.com>
2019-03-12 19:09:03 +0530

    Set up basic POST endpoint | Started messages dict

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ app.use(express.urlencoded({extended: true}))
 
 const portNum = 3000
 
+const messages = {
+    'invalidURL': 'invalid URL!'
+}
+
 app.get('*', (req, res) => {
-    res.send('Autonomous Parking Lot System')
+    var res_body = {
+        'msg': messages['invalidURL']
+    }
+    res.send(res_body)
+})
+
+app.post('*', (req, res) => {
+    var res_body = {
+        'msg': 'invalid URL!'
+    }
+    res.send(res_body)
 })
 
 app.listen(portNum)
