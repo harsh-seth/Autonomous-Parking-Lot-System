48bd39f4440cee93ac187d8027fc9d4409302aed
ShashiSekhar <31887443+ShashiSekhar@users.noreply.github.com>
2019-04-03 04:52:28 +0530

    Update working_comm.ino
    Added software serial to ensure easy arduino rewriting

--- terminal/working_comm.ino
+++ terminal/working_comm.ino
@@ -1,3 +1,7 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2,3); // RX, TX
+//const int btPower = 8;
 String readString;
 const int trigPin1 = 12;
 const int echoPin1 = 13;
@@ -7,24 +11,25 @@ long duration1;
 int distance1;
 long duration2;
 int distance2;
-void setup()
+void setup()  
 {
+//  pinMode(btPower,OUTPUT);
+  //digitalWrite(btPower,LOW);
   pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
   pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);  // initialize serial communications at 9600 bps
+  mySerial.begin(9600);
 }
 
 void loop()
 {
-  while(!Serial.available()) {}
-  while (Serial.available())
-  {
-    if (Serial.available() >0)
+  
+    if (mySerial.available() >0)
     {
-      readString  = Serial.readString(); //makes the string readString
-    }
+      readString  = mySerial.readString(); //makes the string readString
+    
 
   // Clears the trigPin
   digitalWrite(trigPin1, LOW);
@@ -53,9 +58,13 @@ void loop()
   String distance = String(distance1+distance2);
   String payLoad = (distance + "," + readString);
   Serial.println(payLoad);
-  }
+//  delay(500);
+//  digitalWrite(btPower,HIGH);
+//  delay(500);
+//  digitalWrite(btPower,LOW);
   delay(500);
 
   // serial write section
-  Serial.flush();
+  //Serial.flush();
+    }
 }
